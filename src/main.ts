53d6a950b29b6600610bc65e1eeb382c5d665b50
Omar <omardzc94@gmail.com>
2025-09-16 12:46:45 +0200

    añadir vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
+// Inject Vercel Analytics BEFORE bootstrapping
+inject();
+injectSpeedInsights();
+
+// Bootstrap the application
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
